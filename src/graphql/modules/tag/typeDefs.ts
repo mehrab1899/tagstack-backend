3b23fbe08090dccd2f02c8ebc08a1efe520d3102
mehrab1899 <mehrabmehmood1899@gmail.com>
2025-06-10 11:21:28 +0100

    feat(tag): define GraphQL types and mutations for Tag with createTag and tags query

--- src/graphql/modules/tag/typeDefs.ts
+++ src/graphql/modules/tag/typeDefs.ts
@@ -1,1 +1,19 @@
-//typeDef for tag
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Tag {
+    id: ID!
+    name: String!
+    createdAt: String!
+  }
+
+  extend type Query {
+    tags: [Tag!]!
+  }
+
+  extend type Mutation {
+    createTag(name: String!): Tag!
+  }
+`;
+
+export default typeDefs;
