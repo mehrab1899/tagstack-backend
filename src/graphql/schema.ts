8d9fc6b90147f153a719f90af8e5b812bcedf4e4
mehrab1899 <mehrabmehmood1899@gmail.com>
2025-06-11 12:58:10 +0100

    chore(schema): register tag module typeDefs and resolvers into unified GraphQL schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -2,8 +2,10 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 
 import authTypeDefs from './modules/auth/typeDefs';
 import authResolvers from './modules/auth/resolvers';
+import tagResolvers from './modules/tag/resolvers';
+import tagTypeDefs from './modules/tag/typeDefs';
 
 export const schema = makeExecutableSchema({
-  typeDefs: [authTypeDefs],
-  resolvers: [authResolvers],
+  typeDefs: [authTypeDefs,tagTypeDefs],
+  resolvers: [authResolvers,tagResolvers],
 });
